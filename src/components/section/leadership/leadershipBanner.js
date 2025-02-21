a4a8d32cbdf0645508cdb26f3e70998f2f510bb8
yash <yashporwal208@gmail.com>
2025-02-21 12:32:09 +0530

    image changes

--- src/components/section/leadership/leadershipBanner.js
+++ src/components/section/leadership/leadershipBanner.js
@@ -3,6 +3,9 @@ import Banner from "@/components/common/MainBanner/Banner";
 import React, { useState } from "react";
 import bannerImage from "@/assests/leadership/LeaderBanner.jpg";
 import Navbar from "@/components/layout/navbar/Navbar";
+import featherImage from "@/assests/leadership/feather.png";
+import Image from "next/image";
+
 function LeadershipBanner() {
   const [activeSlide, setActiveSlide] = useState(0);
 
@@ -15,8 +18,15 @@ function LeadershipBanner() {
       image: bannerImage,
       data: (
         <div className="absolute md:right-20 text-center text-white z-20 xl:w-[40%] lg:w-[50%] flex flex-col gap-5 px-3">
-          <p className="md:text-7xl text-6xl">
+          <p className="md:text-7xl text-6xl relative">
             कर्मण्येवाधिकारस्ते मा फलेषु कदाचन{" "}
+            <div className="absolute -top-[3rem] transform -translate-y-1/2 -left-5">
+              <Image
+                src={featherImage}
+                alt="Peacock feather"
+                className="w-[15rem] h-[15rem]"
+              />
+            </div>
           </p>
           <p>The Bhagavad Gita, Chapter II, Verse 47 </p>
           <p className="text-3xl mt-5">
