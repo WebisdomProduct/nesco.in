0a37bc5be8faa340f20a18a099647b756d6131d4
KESHUKUMAR1909 <keshukumar1909@gmail.com>
2026-06-09 10:33:49 +0530

    resolving the qc changes of the 9 june

--- src/components/OldSiteFrame.jsx
+++ src/components/OldSiteFrame.jsx
@@ -4,33 +4,44 @@ import Navbar from '@/components/layout/navbar/Navbar';
 import Footer from '@/components/layout/footer/footer';
 
 export default function OldSiteFrame({ src }) {
+  const isDisclosurePage =
+    src ===
+    'https://nesco.in/disclosures-under-regulation-46-of-sebi-(lodr)-regulations-2015.php';
+
+  const isStockInfoPage =
+    src === 'https://nesco.in/stock-information';
+
   return (
     <>
       <Navbar activeSlide={1} />
-      <div style={{
-        width: '100%',
-        height: 'calc(100vh - 90px)', // keep consistent container height
-        overflow: 'hidden',
-        position: 'relative',
-        marginTop: '90px', // Standard fixed navbar space
-      }}>
+
+      <div
+        style={{
+          width: '100%',
+          height: 'calc(100vh - 90px)',
+          overflow: 'hidden',
+          position: 'relative',
+          marginTop: '90px',
+        }}
+      >
         <iframe
           src={src}
           frameBorder="0"
           scrolling="yes"
           style={{
             width: '100%',
-            height: src === 'https://nesco.in/disclosures-under-regulation-46-of-sebi-(lodr)-regulations-2015.php'
-              ? 'calc(100% + 250px + 700px)' // clip 250px top + 800px bottom
-              : 'calc(100% + 85px + 790px)', // clip 85px top + 800px bottom
-            marginTop: src === 'https://nesco.in/disclosures-under-regulation-46-of-sebi-(lodr)-regulations-2015.php'
-              ? '-150px' // hide both main and secondary header
-              : '-85px', // hide main header
+            height: isDisclosurePage
+              ? 'calc(100% + 250px + 700px)'
+              : isStockInfoPage
+              ? 'calc(100% + 85px + 600px)' // reduced bottom space
+              : 'calc(100% + 85px + 790px)',
+            marginTop: isDisclosurePage ? '-150px' : '-85px',
             border: 'none',
-            display: 'block'
+            display: 'block',
           }}
         />
       </div>
+
       <Footer />
     </>
   );
